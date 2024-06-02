85acae376f5cf9e7789a4f89db7b4d1221f303bb
Kartvya69 <148194098+Kartvya69@users.noreply.github.com>
2024-06-02 17:57:44 +0530

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,6 +1,7 @@
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
+sudo apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.bashrc
 nvm install 20
