57f61ec493692fef3ddbd66eae6ad72f236ffbc0
Kartvya69 <148194098+Kartvya69@users.noreply.github.com>
2024-06-02 17:57:09 +0530

    Create nodejs.sh

---
+++ nodejs.sh
@@ -0,0 +1,6 @@
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install 20
